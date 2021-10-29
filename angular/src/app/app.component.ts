373027348d4b4be5b11707d068411d2deec27a96
Tarsis de Lima <br.tmvdl@gmail.com>
2021-10-29 16:01:54 -0300

    Adding parcial layout in Angular project

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
 
+type ContactType = 'phone' | 'whatsapp' | 'email' | 'facebook' | 'linkedin'
+
+class ExperienceModel {
+  public constructor(
+    public job_type: 'clt' | 'pj' = 'clt',
+    public job_name: string = '',
+    public employee_name: string = '',
+    public address: string = '',
+    public date_begin: string = '',
+    public date_end: string = 'present'
+  ) { }
+}
+
+class FormationModel {
+  public constructor(
+    public class_name: string = '',
+    public school_name: string = '',
+    public address: string = '',
+    public date_begin: string = '',
+    public date_end: string = 'present'
+  ) { }
+}
+
+class ContactModel {
+  public constructor(
+    public type: ContactType = 'phone',
+    public value: string = ''
+  ) { }
+}
+
+class FormModel {
+  public constructor(
+    public name: string = '',
+    public address: string = '',
+    public birthday: string = '',
+    public contact_list: ContactModel[] = [new ContactModel('phone')],
+    public experience_list: ExperienceModel[] = [new ExperienceModel()],
+    public formation_list: FormationModel[] = [new FormationModel()]
+  ) { }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pdf-angular';
+  public form: FormModel = new FormModel();
+
+  public job_types: any[] = [
+    { key: 'clt', value: 'CLT' },
+    { key: 'pj', value: 'PJ' }
+  ]
+
+  public contact_types: any[] = [
+    { key: 'phone', value: 'Telefone' },
+    { key: 'whatsapp', value: 'WhatsApp' },
+    { key: 'email', value: 'E-mail' },
+    { key: 'facebook', value: 'Facebook' },
+    { key: 'linkedin', value: 'Linkedin' },
+  ]
+
+  public downloadPDF(): void {
+  }
 }
