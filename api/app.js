2028325ff4c4f912bb82a34903e95a6a39c26caa
Tarsis de Lima <br.tmvdl@gmail.com>
2021-11-05 21:08:59 -0300

    Removing comments in API project

--- api/app.js
+++ api/app.js
@@ -1,14 +1,10 @@
 const cors = require('cors')
 const express = require('express')
-// const bodyParser = require('body-parser')
 const app = express()
 
 app.use(cors({ methods: 'GET,POST', origin: '*' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/v1', require('./routes.v1'))
 
