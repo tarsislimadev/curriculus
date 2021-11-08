dfec64f50cb2e3472cc9a4b8e33081a2f29c1a3a
Tarsis de Lima <br.tmvdl@gmail.com>
2021-11-08 11:45:28 -0300

    Adding values bindings in form fields

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -8,19 +8,20 @@ class ExperienceModel {
     public job_type: JobType = 'clt',
     public job_name: string = '',
     public employee_name: string = '',
-    public address: string = '',
-    public date_begin: string = '',
-    public date_end: string = 'present'
+    public address: CityModel = new CityModel(),
+    public date_begin: DateFieldModel = new DateFieldModel(),
+    public date_end: DateFieldModel = new DateFieldModel()
   ) { }
 }
 
 class FormationModel {
   public constructor(
+    public type: string = 'n1',
     public class_name: string = '',
     public school_name: string = '',
-    public address: string = '',
-    public date_begin: string = '',
-    public date_end: string = 'present'
+    public address: CityModel = new CityModel(),
+    public date_begin: DateFieldModel = new DateFieldModel(),
+    public date_end: DateFieldModel = new DateFieldModel()
   ) { }
 }
 
@@ -31,12 +32,43 @@ class ContactModel {
   ) { }
 }
 
+class CityModel {
+  public constructor(
+    public city: string = '',
+    public state: string = 'SP'
+  ) { }
+}
+
+class NeighboorModel extends CityModel {
+  public constructor(
+    public neighboor: string = '',
+    public city: string = '',
+    public state: string = 'SP'
+  ) {
+    super(city, state)
+  }
+}
+
+class DateFieldModel {
+
+  public value: string = ''
+
+  public constructor(
+    public date: string = '',
+    public month: string = '',
+    public year: string = ''
+  ) {
+  }
+}
+
 class FormModel {
   public constructor(
     public name: string = '',
-    public address: string = '',
-    public birthday: string = '',
-    public contact_list: ContactModel[] = [new ContactModel('phone')],
+    public address: NeighboorModel = new NeighboorModel(),
+    public birthday: DateFieldModel = new DateFieldModel(),
+    public marital_status: string = 'single',
+    public children_number: string = '',
+    public contact_list: ContactModel[] = [new ContactModel('email'), new ContactModel('phone')],
     public experience_list: ExperienceModel[] = [new ExperienceModel()],
     public formation_list: FormationModel[] = [new FormationModel()]
   ) { }
@@ -48,6 +80,15 @@ class FormModel {
 })
 export class AppComponent {
   public form: FormModel = new FormModel();
+
+  public formation_types: any[] = [
+    { key: 'n1', value: 'Bacharelado' },
+    { key: 'n2', value: 'Licenciatura' },
+    { key: 'n3', value: 'Mestrado' },
+    { key: 'n4', value: 'Doutorado' },
+    { key: 'n5', value: 'Especialização' },
+    { key: 'n6', value: 'Curso técnico' },
+  ]
 
   public job_types: any[] = [
     { key: 'clt', value: 'CLT' },
@@ -63,20 +104,28 @@ export class AppComponent {
   ]
 
   public personal_status_types: any[] = [
-    { key: 'single', value: 'Solteiro' },
+    { key: 'single', value: 'Solteiro(a)' },
+    { key: 'maried', value: 'Casado(a)' },
+    { key: 'separated', value: 'Divorciado(a)' },
+    { key: 'union', value: 'União estável' },
+    { key: 'other', value: 'Outro' }
   ]
 
-  public addExperience (): boolean {
+  public states: any[] = [
+    { key: 'SP', value: 'São Paulo' },
+  ]
+
+  public addExperience(): boolean {
     this.form.experience_list.push(new ExperienceModel())
     return false
   }
 
-  public addFormation (): boolean {
+  public addFormation(): boolean {
     this.form.formation_list.push(new FormationModel())
     return false
   }
 
-  public addContact (): boolean {
+  public addContact(): boolean {
     this.form.contact_list.push(new ContactModel('phone'))
     return false
   }
