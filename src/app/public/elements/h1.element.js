34bdbc2ece7d96cef013cbf464838322c3909957
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-14 20:27:37 +0000

    Add some elements files

---
+++ src/app/public/elements/h1.element.js
@@ -0,0 +1,7 @@
+import { TextElement } from './text.element.js'
+
+export class H1Element extends TextElement {
+  getTagName() { return 'h1' }
+
+  getName() { return 'h1' }
+}
---
+++ src/app/public/elements/h3.element.js
@@ -0,0 +1,7 @@
+import { TextElement } from './text.element.js'
+
+export class H3Element extends TextElement {
+  getTagName() { return 'h3' }
+
+  getName() { return 'h3' }
+}
