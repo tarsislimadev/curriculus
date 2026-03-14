22f5de1ab611de78a9d63a2a535fdeae1dbed241
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-14 19:23:46 +0000

    Remove useless files

--- src/angular/src/app/app-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { routes } from './app-routes'
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
