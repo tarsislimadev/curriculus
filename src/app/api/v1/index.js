e895ae5cce74ed7aa6467168ce62fe80b2e9c338
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-14 19:38:20 +0000

    Fix project paths

--- src/app/api/v1/index.js
+++ src/app/api/v1/index.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 const pdf = require('html-pdf')
-const package = require('./package.json')
+const package = require('../../package.json')
 const express = require('express')
 const router = express.Router()
 const { API_URL } = process.env
-const public_path = path.resolve('.', 'public')
+const public_path = path.resolve('..', '..', 'files')
 let counter = 0
 
 const makeName = () => path.resolve(public_path, ['_', Date.now(), '+', ++counter, '.pdf'].join(''))
--- src/app/app.js
+++ src/app/app.js
@@ -2,10 +2,10 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
 app.use(cors({ methods: 'GET,POST', origin: '*' }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-app.use('/v1', require('./routes.v1'))
+app.use('/v1', require('./api/v1'))
 
 module.exports = app
